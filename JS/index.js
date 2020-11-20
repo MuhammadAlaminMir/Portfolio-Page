5707cd09c2e6a8f7c2a793f7c565a0718b464024
alaminmir08 <alaminmir08@gamil.com>
2020-11-20 18:46:36 +0600

    added pranto vai photo

--- JS/index.js
+++ JS/index.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         }
     })
     let typed = new Typed(".element", {
-        strings: ["Alamin Mir", "A Student", "A Programmer", "A Web Developer"],
+        strings: ["Shahriar Pranto", "a Student","a Civil Engineer", "a Architectural Designer" ],
         smartBackspace: true,
         typeSpeed:100,
         backSpeed:100,
@@ -22,11 +22,11 @@ $(document).ready(function(){
         element: document.getElementById('exp-id'),
         handler: function() {
           let p = document.querySelectorAll('.progress-bar');
-    p[0].setAttribute('style', 'width:80%;transition:1s all');
-    p[1].setAttribute('style', 'width:70%;transition:1.5s all');
-    p[2].setAttribute('style', 'width:75%;transition:1.7s all');
+    p[0].setAttribute('style', 'width:75%;transition:1s all');
+    p[1].setAttribute('style', 'width:80%;transition:1.5s all');
+    p[2].setAttribute('style', 'width:85%;transition:1.7s all');
     p[3].setAttribute('style', 'width:60%;transition:2s all');
-    p[04].setAttribute('style', 'width:50%;transition:2.3s all');
+    p[04].setAttribute('style', 'width:55%;transition:2.3s all');
         }
         ,offset:'90%'
       })
